df5899a252197c1cc6e97971c46946eb356b4154
Sunny-JP <kajimalu10@gmail.com>
2024-05-08 23:30:01 +0900

    Add files

---
+++ games/gsnavi/js/routes.js
@@ -0,0 +1,88 @@
+
+var routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/kigan/',
+    url: './pages/01kigan.html',
+  },
+  {
+    path: '/build/',
+    url: './pages/02build.html',
+  },
+  {
+    path: '/calc/',
+    url: './pages/03calc.html',
+  },
+  {
+    path: '/encn/',
+    url: './pages/04encn.html',
+  },
+  {
+    path: '/dic/',
+    url: './pages/05dic.html',
+  },
+  {
+    path: '/cal/',
+    url: './pages/06cal.html',
+  },
+
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    componentUrl: './pages/dynamic-route.html',
+  },
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = to.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            componentUrl: './pages/request-and-load.html',
+          },
+          {
+            props: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
